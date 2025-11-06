90d12a474f3a843039b252d7d5e277a03a2104e5
aummpatel <aum.patel@torontomu.ca>
2025-11-05 23:02:27 -0500

    Added Confusion Matrix on Test Results

--- AER850_Project2_Step_5.py
+++ AER850_Project2_Step_5.py
@@ -3,6 +3,9 @@ import matplotlib.pyplot as plt
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import ImageDataGenerator as IDG
+from sklearn.metrics import confusion_matrix, classification_report
+import seaborn as sns
+import pandas as pd
 
 # Relative paths for test images and model
 test_images = [
@@ -10,6 +13,7 @@ test_images = [
     ("missing-head","Project 2 Data/Data/test/missing-head/test_missinghead.jpg"),
     ("paint-off","Project 2 Data/Data/test/paint-off/test_paintoff.jpg")]
 model_path = "Best_Models/overall_best_model.keras"
+print("Loaded Best Model successfully")
 
 best_model = load_model(model_path) # Load the best model
 class_labels = ['crack','missing-head','paint-off']
@@ -45,3 +49,20 @@ test_loss, test_accuracy = best_model.evaluate(test_gen, verbose=1)
 
 print(f"\nFinal Test Accuracy : {test_accuracy:.4f}")
 print(f"Final Test Loss     : {test_loss:.4f}\n")
+
+# Confusion matrix for test results to observe edge cases
+y_true = test_gen.classes #True Labels
+y_prob = best_model.predict(test_gen, verbose=0)
+y_pred = y_prob.argmax(1) #Predicated Labels
+
+cm = cm = confusion_matrix(y_true, y_pred)
+cm_df = pd.DataFrame(cm, index=class_labels, columns=class_labels)
+
+plt.figure(figsize=(6,5))
+sns.heatmap(cm_df, annot=True, fmt='d', cmap='Blues', cbar=False,
+            annot_kws={"size": 13, "weight": "bold"})
+plt.title('Confusion Matrix for Tuned Model', fontsize=14, weight='bold')
+plt.xlabel('Predicted Label')
+plt.ylabel('True Label')
+plt.tight_layout()
+plt.show()
